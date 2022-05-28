261656129c5e2133d1a7fd078afa0318f65e17ec
sol-arnal-tdf <104174628+sol-arnal-tdf@users.noreply.github.com>
2022-05-27 21:29:18 -0300

    arreglo que dejo pierina no lo finalice

--- src/tareaAlumnos.ts
+++ src/tareaAlumnos.ts
@@ -13,30 +13,40 @@ function ingresar_datos(): void {
   }
 }
 
-function prom_Anual(): void {
-  for (let ind: number = 0; ind < cantidad; ind++) {
-    let resultado: number = 0;
-    resultado = (nota1[ind] + nota2[ind] + nota3[ind]) / 3;
-    console.log("El promedio del alumno : ", nAlumnos[ind]);
-    console.log("es de : ", resultado);
+function prom_Anual(ind: number): void {
+  let resultado: number = 0;
+  resultado = (nota1[ind] + nota2[ind] + nota3[ind]) / 3;
+  console.log("El promedio del alumno : ", nAlumnos[ind]);
+  console.log("es de : ", resultado);
 
-    console.log(
-      "El promedio se da en base a la sumatoria de : ",
-      nota1[ind],
-      " + ",
-      nota2[ind],
-      " + ",
-      nota3[ind],
-      "dividido 3 (la cantidad de notas a promediar)"
-    );
+  console.log(
+    "El promedio se da en base a la sumatoria de : ",
+    nota1[ind],
+    " + ",
+    nota2[ind],
+    " + ",
+    nota3[ind],
+    "dividido 3 (la cantidad de notas a promediar)"
+  );
+}
+
+function buscar_alumno(nombre_buscado: string): void {
+  for (let indi: number = 0; indi < cantidad; indi++) {
+    if (nAlumnos[indi] === nombre_buscado) {
+      return indi;
+    }
   }
+  return;
 }
 let cantidad: number = Number(
   prompt("ingrese la cantidad de alumnos a procesar : ")
 );
-let nAlumnos: number = new Array(cantidad);
+let nAlumnos: string = new Array(cantidad);
 let nota1: number = new Array(cantidad);
 let nota2: number = new Array(cantidad);
 let nota3: number = new Array(cantidad);
 ingresar_datos();
-prom_Anual();
+let nombre_buscado: string = prompt(
+  "Ingrese el nombre del alumno a buscar  : "
+);
+prom_Anual(nombre_buscado);
